648e7565dc68541797080734a53bd2288351147b
Ishtiaque Khan <ishtiaque.khan@newscred.com>
2021-01-04 14:42:56 +0600

    Ghosha maja

--- front-end/code/pages/index.tsx
+++ front-end/code/pages/index.tsx
@@ -4,7 +4,7 @@ import { NextPage } from 'next';
 import { Game, Layout } from '@components';
 
 const Index: NextPage = () => (
-  <Layout pageTitle="First Post">
+  <Layout pageTitle="Tic Tac Toe">
     <Game />
   </Layout>
 );
