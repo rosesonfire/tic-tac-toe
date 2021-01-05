89e963767d90d0900aa89a28dd6ae5fd02a82186
Ishtiaque Khan <ishtiaque.khan@newscred.com>
2021-01-05 20:23:25 +0600

    Add production build result

---
+++ front-end/code/out/_next/static/lzMcX7y8V7LAcI-HbQ0j6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F66b2cdfe221bf6dcfb3d.css","static\u002Fchunks\u002Fpages\u002Findex-b3f6756e605d0e254491.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1f16d1286e266dc1cfdc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
